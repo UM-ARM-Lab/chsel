b16129a12b113ece2c4b2853e1997c04bf73cb0f
Sheng Zhong <zhsh@umich.edu>
2023-03-08 18:38:03 -0500

    Reinitialize transform estimates around elites

--- src/chsel/__init__.py
+++ src/chsel/__init__.py
@@ -2,3 +2,6 @@ from chsel.wrapper import CHSEL
 from chsel.types import Semantics, SemanticsClass
 from chsel.registration_util import solution_to_world_to_link_matrix, apply_similarity_transform
 from chsel.costs import VolumetricCost, VolumetricDirectSDFCost, VolumetricDoubleDirectCost
+from chsel.initialization import reinitialize_transform_estimates, random_rotation_perturbations, \
+    reinitialize_transform_around_elites
+from chsel.conversion import continuous_representation_to_RT, RT_to_continuous_representation
